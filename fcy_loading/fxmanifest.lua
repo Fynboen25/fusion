3d1aa1b7b7e49d3b6db6337d0fc3b12dbb32f4e2
Fynboen25 <137529862+Fynboen25@users.noreply.github.com>
2024-02-11 11:05:32 +0100

    Fusion City - Loading screen

---
+++ fcy_loading/fxmanifest.lua
@@ -0,0 +1,21 @@
+description 'Renox loadingscreen'
+author 'FuTTiiZ'
+version '1.0.0'
+lua54 'yes'
+game 'gta5'
+fx_version 'cerulean'
+
+files {
+    'index.html',
+    'fonts/**',
+    'media/**',
+    'css/**',
+    'js/**'
+}
+
+client_script 'client.lua'
+
+loadscreen 'index.html'
+loadscreen_manual_shutdown 'yes'
+dependencies '/assetpacks'
+dependency '/assetpacks'
